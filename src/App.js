4d7fe7d348ad21dc2a801b483e691422b61352b5
Audun Halland <audun.halland@gmail.com>
2018-10-11 01:42:11 +0200

    Add all resources and simple list view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
 import * as ra from 'react-admin';
+import buildOpenCrudProvider from 'ra-data-opencrud';
 
-const App = () => (
-  <ra.Admin />
+const FilmList = (props) => (
+  <ra.List {...props}>
+    <ra.Datagrid>
+      <ra.TextField source="title" />
+    </ra.Datagrid>
+  </ra.List>
 );
+
+const NameList = (props) => (
+  <ra.List {...props}>
+    <ra.Datagrid>
+      <ra.TextField source="name" />
+    </ra.Datagrid>
+  </ra.List>
+);
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { dataProvider: null };
+  }
+  componentDidMount() {
+    buildOpenCrudProvider({ clientOptions: { uri: 'http://localhost:4466/' } })
+      .then(dataProvider => this.setState({ dataProvider }));
+  }
+
+  render() {
+    const { dataProvider } = this.state;
+
+    if (!dataProvider) {
+      return <div>Loading</div>;
+    }
+
+    return (
+      <ra.Admin dataProvider={dataProvider}>
+        <ra.Resource name="Planet" list={NameList} />
+        <ra.Resource name="Lifeform" list={NameList} />
+        <ra.Resource name="Character" list={NameList} />
+        <ra.Resource name="Starship" list={NameList} />
+        <ra.Resource name="Vehicle" list={NameList} />
+        <ra.Resource name="Film" list={FilmList} />
+      </ra.Admin>
+    );
+  }
+}
 
 export default App;
